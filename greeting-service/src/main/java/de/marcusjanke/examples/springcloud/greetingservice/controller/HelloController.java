7b0318a43169c49cc19384432dc95a60260daf96
marcus-j <mj@marcusjanke.de>
2018-05-13 18:31:57 +0200

    minor clean up and added docker-compose

--- greeting-service/src/main/java/de/marcusjanke/examples/springcloud/greetingservice/controller/HelloController.java
+++ greeting-service/src/main/java/de/marcusjanke/examples/springcloud/greetingservice/controller/HelloController.java
@@ -1,11 +1,11 @@
 package de.marcusjanke.examples.springcloud.greetingservice.controller;
 
+import de.marcusjanke.examples.springcloud.greetingservice.services.NewsServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import de.marcusjanke.examples.springcloud.greetingservice.services.NewsServiceClient;
 
 @RestController
 public class HelloController {
